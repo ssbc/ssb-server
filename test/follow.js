87273577e3a68900659a79b3382dac61195e8330
Paul Frazee <pfrazee@gmail.com>
2014-11-22 11:40:26 -0600

    bring test/follow back to where it was

--- test/follow.js
+++ test/follow.js
@@ -1,44 +1,55 @@
 var tape = require('tape')
 var pull = require('pull-stream')
 var sbot = require('../')
+var seal = require('../lib/seal')(require('ssb-keys'))
 
 tape('follow, isFollowing, followedUsers, unfollow', function (t) {
   var u = require('./util')
-  var bob = '5f3eac42ae28da590889f68c9de75e3bf2c1194c8a6257a020a05b2bf42742de'
-  var alice = '75e3bf2c1194c8a6257a020a05b2bf42742de5f3eac42ae28da590889f68c9de'
+
+  var dbAlice = u.createDB('followtest-alice')
+  var alice = dbAlice.createFeed()
+
+  var dbBob = u.createDB('followtest-bob')
+  var bob = dbBob.createFeed()
 
   var db = u.createDB('feed-test')
   var feed = db.createFeed()
   var server = sbot({port: 1234, host: 'localhost', pass: 'foo'}, db, feed)
-  var client = sbot.connect({port: 1234, host: 'localhost'})
+  var client = sbot.createClient({port: 1234, host: 'localhost'})
 
-  client.auth('foo', function(err) {
+  var signed = seal.sign(feed.keys, {
+    role: 'client',
+    ts: Date.now(),
+    public: feed.keys.public
+  })
+
+  client.auth(signed, function(err) {
     if (err) throw err
   
-    client.follow(bob, function(err) {
+    client.follow(bob.id, function(err, msg) {
       if (err) throw err
       
-      client.isFollowing(bob, function(err, isFollowing) {
+      client.isFollowing(bob.id, function(err, isFollowing) {
         if (err) throw err
         t.equal(isFollowing, true)
 
-        client.isFollowing(alice, function(err, isFollowing) {
+        client.isFollowing(alice.id, function(err, isFollowing) {
           if (err) throw err
           t.equal(isFollowing, false)
 
           pull(client.followedUsers(), pull.collect(function(err, users) {
             if (err) throw err
             t.equal(users.length, 1)
-            t.equal(users[0].toString('hex'), bob)
+            t.equal(users[0].toString('base64'), bob.id.toString('base64'))
 
-            client.unfollow(bob, function(err) {
+            client.unfollow(bob.id, function(err) {
               if (err) throw err
 
-              client.isFollowing(bob, function(err, isFollowing) {
+              client.isFollowing(bob.id, function(err, isFollowing) {
                 if (err) throw err
                 t.equal(isFollowing, false)
                 t.end()
-                client.socket.close()
+                client.close(console.log)
                 server.close()
               })
             })
