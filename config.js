a683a29cdb14e2c1b3d6b8d93c035541e6eb519f
Dominic Tarr <dominic.tarr@gmail.com>
2014-12-09 21:54:04 -0800

    default timeout to 30 seconds

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ var path = require('path')
 module.exports = require('rc')('ssb', {
   hostname: '',
   port: 2000,
-  timeout: 3000,
+  timeout: 30000,
   path: path.join(process.env.HOME, '.ssb')
 })
