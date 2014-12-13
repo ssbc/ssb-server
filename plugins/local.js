c48b61d8684871762e8b7d5a203d32b7bf22d620
Dominic Tarr <dominic.tarr@gmail.com>
2014-12-13 13:52:50 -0800

    make local plugin into a propper plugin with an api

--- plugins/local.js
+++ plugins/local.js
@@ -1,44 +1,63 @@
 
-
 var broadcast = require('broadcast-stream')
 
-module.exports = function local (server) {
+function isFunction (f) {
+  return 'function' === typeof f
+}
 
-  var local = broadcast(server.config.port)
+module.exports = {
+  name: 'local',
+  version: '1.0.0',
+  manifest: {
+    get: 'async'
+  },
+  init: function (server) {
 
-  var id = server.feed.id
-  var peers = {}
-  function toArray (o) {
-    return Object.keys(o).map(function (k) { return o[k] })
+    var local = broadcast(server.config.port)
+
+    var id = server.feed.id
+    var peers = {}
+    function toArray (o) {
+      return Object.keys(o).map(function (k) { return o[k] })
+    }
+
+    local.on('data', function (buf) {
+      if(buf.loopback) return
+
+      var data = JSON.parse(buf.toString())
+      data.host = buf.address
+      var ts = Date.now()
+      data.ts = ts
+      var isNew = (data.id in peers)
+      peers[data.id] = data
+
+      if (!isNew)
+        server.emit('log:info', ['local', null, 'discovered', data])
+      server.emit('local', data)
+    })
+
+    setInterval(function () {
+      // broadcast self
+      local.write(JSON.stringify({id: id, port: server.config.port}))
+
+      // clean out expired entries
+      var ts = Date.now()
+      for(var k in peers) {
+        if(peers[k].ts + 3000 < ts)
+          delete peers[k]
+      }
+      server.localPeers = toArray(peers)
+    }, 1000)
+
+    return {
+      get: function (cb, cb2) {
+        if(isFunction(cb)) cb(null, peers)
+        else if(isFunction(cb2)) cb2(null, peers)
+        else return peers
+      }
+    }
+
   }
 
-  local.on('data', function (buf) {
-    if(buf.loopback) return
-
-    var data = JSON.parse(buf.toString())
-    data.host = buf.address
-    var ts = Date.now()
-    data.ts = ts
-    var isNew = (data.id in peers)
-    peers[data.id] = data
-
-    server.localPeers = toArray(peers)
-    if (isNew)
-      server.emit('log:info', ['local', null, 'discovered', data])
-    server.emit('local', data)
-  })
-
-  setInterval(function () {
-    // broadcast self
-    local.write(JSON.stringify({id: id, port: server.config.port}))
-
-    // clean out expired entries
-    var ts = Date.now()
-    for(var k in peers) {
-      if(peers[k].ts + 3000 < ts)
-        delete peers[k]
-    }
-    server.localPeers = toArray(peers)
-  }, 1000)
-
 }
+
