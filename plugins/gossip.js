6398a88f649b1e922dc4134c43a85bde6e4f9de6
Dominic Tarr <dominic.tarr@gmail.com>
2014-12-28 17:37:16 +1300

    tidy up gossip plugin

--- plugins/gossip.js
+++ plugins/gossip.js
@@ -1,73 +1,8 @@
+'use strict'
 var pull = require('pull-stream')
 var toAddress = require('../lib/util').toAddress
 var nonPrivate = require('non-private-ip')
 var u = require('../lib/util')
-
-/*
-
-Track available peers, and what keys are reachable at them.
-
-We want to make sure that we don't connect to one node
-more than once.
-
-It's easier to tell the remote connection by it's key,
-because that is sent with the message, so, on connecting
-outwards to a node, remember the address -> key.
-
-So, when connecting to another node, check if you are already
-connected to that key.
-
-for an outgoing connection, remember which id it has,
-and which ip:port it has.
-
-for an incoming connection, check if you know that id,
-and it's ip.
-
-If you get an incomming connection, but have not connected
-out to it yet, then you will not know what port it hosts on,
-so you might connect to it twice, but this will only be a problem
-early in a processes' life.
-
-So, build up a table of ids and their addresses,
-
-and a table of current connections,
-and which id it is a connection to.
-
-Also, for wifi networks, id->ip can change,
-also it could work for a bit and then leave.
-You could even have a new device join a network
-and get the same ip as a previous device. (rare, but possible)
-
-okay but local network is advertised by udp,
-and that can be used to update the records.
-
-Seeds: there are some peers that we know about when starting up.
-probably want to connect to these rapidly, to check they are alive.
-then, backoff on peers that are old.
-
----
-
-if there have been no connection attempts to a seed
-  set high priority
-
-if a connection failed
-  half priority
-
-if a connection timed out
-  reduce priority 10%
-
-if a connection succeed after failing
-  set high priority
-
----
-
-randomly connect to peers
-
-if a connection fails,
-set an exponential backoff?
-
-
-*/
 
 function all(stream, cb) {
   if (cb) return pull(stream, pull.collect(cb))
@@ -84,15 +19,14 @@ function isFunction (f) {
   return 'function' === typeof f
 }
 
-function rand(array) {
-  return array[~~(Math.random()*array.length)]
+var isArray = Array.isArray
+
+function isObject (o) {
+  return o && 'object' === typeof o
 }
 
-function reduce(obj, reduce, init) {
-  var acc = init
-  for(var k in obj)
-    acc = reduce(acc, obj[k])
-  return acc
+function rand(array) {
+  return array[~~(Math.random()*array.length)]
 }
 
 var DEFAULT_PORT = 2000
@@ -130,7 +64,6 @@ function seeds (server, cb) {
     server.ssb.messagesByType('pub'),
     pull.map(function (e) {
       var o = toAddress(e.content.address)
-      o.id = e.content.id || e.author
       return o
     }),
     pull.filter(function (e) {
@@ -165,19 +98,12 @@ function sameHost(e) {
     }
 }
 
-var isArray = Array.isArray
-
-function isObject (o) {
-  return o && 'object' === typeof o
-}
-
 module.exports = {
   name: 'gossip',
   version: '1.0.0',
   manifest: {
     seeds: 'async',
     peers: 'sync',
-    connections: 'sync',
   },
   init: function (server) {
     var sched
@@ -205,13 +131,7 @@ module.exports = {
       peers: function () {
         return peers
       },
-      connections: function () {
-        return connections
-      }
     }
-
-    var connections = {}
-    var scheduled = false
 
     server.on('remote:authorized', function (rpc, authed) {
       //don't track cli/web client connections
@@ -258,14 +178,14 @@ module.exports = {
       }, 500 + ~~(Math.random() * 1000))
     })()
 
-    var current = 0
+    var count = 0
 
     function connect () {
       if(server.closed) return
 
       //two concurrent connections.
-      if(current >= (conf.connections || 2)) return
-      current ++
+      if(count >= (conf.connections || 2)) return
+      count ++
 
       // connect to this random peer
       var p = rand(peers.filter(function (e) {
