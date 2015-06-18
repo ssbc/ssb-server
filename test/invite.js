5876020f798edabe5ae4686b1a37b6a03894366e
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-18 20:36:45 +0200

    update invite tests to use new caps

--- test/invite.js
+++ test/invite.js
@@ -3,93 +3,68 @@ var ssbKeys = require('ssb-keys')
 var tape = require('tape')
 var explain = require('explain-error')
 var pull = require('pull-stream')
-
+var capClient = require('../cap-client')
 tape('test invite api', function (t) {
 
   var u = require('./util')
 
   var sbotA = u.createDB('test-invite-alice', {
     port: 45451, host: '127.0.0.1',
+    timeout: 200,
     allowPrivate: true
   })
 
   var alice = sbotA.feed
   var bob = sbotA.ssb.createFeed() //bob
 
-  var server = sbotA.use(require('../plugins/invite')).use(require('../plugins/friends'))
+  var server = sbotA
+    .use(require('../plugins/invite'))
+    .use(require('../plugins/friends'))
 
   //request a secret that with particular permissions.
 
   var manf = server.getManifest()
   var client = sbot.createClient(alice.keys, manf)
 
+  console.log(alice.keys.public)
+  console.log(ssbKeys.hash(alice.keys.public))
   client({port: 45451, host: 'localhost', key: alice.keys.public},
     function (err, aliceC) {
     if(err) throw err
 
-    var signed = ssbKeys.signObj(alice.keys, {
-      role: 'client',
-      ts: Date.now(),
-      public: alice.keys.public
-    })
+    aliceC.invite.create(1, function (err, invite) {
+      if(err) throw explain(err, 'cannot create invite code')
 
-    aliceC.auth(signed, function (err, authed) {
-      console.log(err, authed)
-      if(err) throw explain(err, 'cannot authorize')
-      t.ok(authed.granted)
-      aliceC.invite.create(1, function (err, invite) {
-        if(err) throw explain(err, 'cannot create invite code')
+      capClient(invite, manf, function (err, capC) {
+        if(err) throw err
 
-        var bobC =
-          sbot.createClient(bob.keys, manf)
-          ({port: 45451, host: 'localhost', key: alice.keys.public},
-            function (err, bobC) {
+        capC.invite.use({
+          feed: bob.id
+        }, function (err, msg) {
+            if(err) throw explain(err, 'bob cannot use invite code')
 
-              bobC.auth(ssbKeys.signObj(bob.keys, {
-                  role: 'client',
-                  ts: Date.now(),
-                  public: bob.keys.public
-                }), function (err, authed) {
-                  if(err) throw explain(err, 'bob cannot auth')
+          pull(
+            aliceC.feedsLinkedToFeed({id: bob.id, rel: 'contact'}),
+            pull.collect(function (err, ary) {
+              if(err) throw err
 
-                  var secret = invite.split(',')[2]
+              var followed = ary[0]
+              delete followed.message
 
-                  var hmacd = ssbKeys.signObjHmac(secret, {
-                      keyId: ssbKeys.hash(secret, 'base64'),
-                      //request must contain your own id,
-                      //which prevents this request from being replayed.
-                      //because the server checks this the authed user.
-                      feed: bob.id,
-                      ts: Date.now()
-                    })
+              t.deepEqual(
+                {source: alice.id, dest: bob.id, rel: 'contact'},
+                ary[0]
+              )
 
-                  bobC.invite.use(hmacd, function (err, msg) {
-                      if(err) throw explain(err, 'bob cannot use invite code')
+              capC.close()
+              aliceC.close()
+              server.close()
+              console.log('done')
+              t.end()
 
-                    pull(
-                      aliceC.feedsLinkedToFeed({id: bob.id, rel: 'contact'}),
-                      pull.collect(function (err, ary) {
-                        if(err) throw err
-                        console.log(ary)
-
-                        var followed = ary[0]
-                        delete followed.message
-
-                        t.deepEqual(
-                          {source: alice.id, dest: bob.id, rel: 'contact'},
-                          ary[0]
-                        )
-                        console.log('CLOSE')
-                        aliceC.close(function () {
-                          server.close()
-                          t.end()
-                        })
-
-                      })
-                    )
-                  })
-              })
             })
+          )
+        })
       })
     })
   })
