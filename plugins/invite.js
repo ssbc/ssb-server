a670466b29b0a3491f8e73c7f3b3ef0f532db837
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-18 20:36:16 +0200

    login via a fixed use cap

--- plugins/invite.js
+++ plugins/invite.js
@@ -26,8 +26,8 @@ module.exports = {
     addMe: 'async'
   },
   permissions: {
-    local: {allow: ['use']},
-    anonymous: {allow: ['use']}
+    master: {allow: ['create']},
+    //temp: {allow: ['use']}
   },
   init: function (server) {
     var codes = {}
@@ -45,52 +45,48 @@ module.exports = {
           return cb(new Error('Server has no public ip address,'
                             + 'cannot create useable invitation'))
 
-        var secret = crypto.randomBytes(32).toString('base64')
-        var keyId = ssbKeys.hash(secret, 'base64')
+        //this stuff is SECURITY CRITICAL
+        //so it should be moved into the main app.
+        //there should be something that restricts what
+        //permissions the plugin can create also:
+        //it should be able to diminish it's own permissions.
+        var seed = crypto.randomBytes(32)
+        var keyCap = ssbKeys.generate('ed25519', seed)
 
-//        codes[keyId] = {
-//          secret: secret, total: n, used: 0
-//        }
-
-        var owner = server.feed
-        codesDB.put(keyId,  {
-          secret: secret, total: +n, used: 0
+        var owner = server.feed.keys.public
+        codesDB.put(keyCap.id,  {
+          public: keyCap.public, total: +n, used: 0,
+                              //TODO: kill "emit"
+                              //(need to figure out what its used for)
+          permissions: {allow: ['emit', 'invite.use'], deny: null}
         }, function (err) {
           if(err) cb(err)
-          else cb(null, [addr, owner.id, secret].join(','))
+          else cb(null, [addr, owner, seed.toString('base64')].join(','))
         })
 
       },
       use: function (req, cb) {
         var rpc = this
-        if(rpc.authorized.hmac)
-          return cb(new Error('cannot use invite code when authorized with hmac'))
 
-        var id = rpc.authorized.id
         server.friends.all('follow', function(err, follows) {
           if (follows && follows[server.feed.id] && follows[server.feed.id][id])
             return cb(new Error('already following'))
 
-          codesDB.get(req.keyId, function(err, invite) {
-
+          codesDB.get(rpc.id, function(err, invite) {
+            if(err) return cb(err)
             // although we already know the current feed
             // it's included so that request cannot be replayed.
             if(!req.feed)
               return cb(new Error('feed to follow is missing'))
 
-            if(req.feed !== id)
-              return cb(new Error('invite code may not be used to follow another key'))
-
-            if(!invite)
-              return cb(new Error('invite code is incorrect or expired'))
-
             if(invite.used >= invite.total)
               return cb(new Error('invite code:'+id+' has expired'))
 
-            if(!ssbKeys.verifyObjHmac(invite.secret, req))
-              return cb(new Error('invalid invite request'))
-
             invite.used ++
+
+            //never allow this to be used again
+            if(invite.used >= invite.total)
+              invite.permissions = {allow: [], deny: true}
 
             //okay so there is a small race condition here
             //if people use a code massively in parallel
@@ -102,78 +98,78 @@ module.exports = {
 
               server.feed.add({
                 type: 'contact',
-                contact: { feed: id },
+                contact: { feed: req.feed },
                 following: true,
                 autofollow: true
               }, cb)
             })
-          }) 
+          })
         })
       },
       addMe: function (req, cb) {
-        if(isString(req)) {
-          req = req.split(',')
-          req = {
-            address: req[0],
-            id: req[1],
-            invite: req[2]
-          }
-        }
-        var rpc = server.connect(toAddress(req.address))
-
-        rpc.once('rpc:unauthorized', function (err) {
-          rpc.close(); cb(err)
-        })
-
-        var remote, auth, done
-
-        rpc.once('remote:authorized', function (res) {
-          remote = true
-          if(remote && done) next()
-        })
-
-        rpc.once('rpc:authorized', function (res) {
-          auth = true; done = rpc.task()
-          if(remote && done) next()
-        })
-
-        function next () {
-          if(rpc.authorized.id !== req.id) {
-            rpc.close()
-            return cb(new Error('pub server did not have correct public key'))
-          }
-          var secret = req.secret || req.invite
-          delete req.invite
-          delete req.secret
-          req.feed = req.feed || server.feed.id
-
-          var invite = ssbKeys.signObjHmac(secret, {
-            keyId: ssbKeys.hash(secret, 'base64'),
-            feed: server.feed.id,
-            ts: Date.now()
-          })
-
-          rpc.invite.use(invite, function (err, res) {
-            if(err) {
-              done(); cb(explain(err, 'invite not accepted'))
-              return
-            }
-            cont.para([
-              server.feed.add({
-                type: 'contact',
-                following: true,
-                contact: { feed: rpc.authorized.id }
-              }),
-              server.feed.add({
-                type: 'pub',
-                address: req.address
-              })
-            ])(function (err, results) {
-              done()
-              cb(err, results)
-            })
-          })
-        }
+//        if(isString(req)) {
+//          req = req.split(',')
+//          req = {
+//            address: req[0],
+//            id: req[1],
+//            invite: req[2]
+//          }
+//        }
+//        var rpc = server.connect(toAddress(req.address))
+//
+//        rpc.once('rpc:unauthorized', function (err) {
+//          rpc.close(); cb(err)
+//        })
+//
+//        var remote, auth, done
+//
+//        rpc.once('remote:authorized', function (res) {
+//          remote = true
+//          if(remote && done) next()
+//        })
+//
+//        rpc.once('rpc:authorized', function (res) {
+//          auth = true; done = rpc.task()
+//          if(remote && done) next()
+//        })
+//
+//        function next () {
+//          if(rpc.authorized.id !== req.id) {
+//            rpc.close()
+//            return cb(new Error('pub server did not have correct public key'))
+//          }
+//          var secret = req.secret || req.invite
+//          delete req.invite
+//          delete req.secret
+//          req.feed = req.feed || server.feed.id
+//
+//          var invite = ssbKeys.signObjHmac(secret, {
+//            keyId: ssbKeys.hash(secret, 'base64'),
+//            feed: server.feed.id,
+//            ts: Date.now()
+//          })
+//
+//          rpc.invite.use(invite, function (err, res) {
+//            if(err) {
+//              done(); cb(explain(err, 'invite not accepted'))
+//              return
+//            }
+//            cont.para([
+//              server.feed.add({
+//                type: 'contact',
+//                following: true,
+//                contact: { feed: rpc.authorized.id }
+//              }),
+//              server.feed.add({
+//                type: 'pub',
+//                address: req.address
+//              })
+//            ])(function (err, results) {
+//              done()
+//              cb(err, results)
+//            })
+//          })
+//        }
       }
     }
   }
