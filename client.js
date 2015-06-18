36400f8bf428c172f17d35019e7a50db5c3c1acb
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-18 14:08:20 +0200

    the client needs to know its keys

--- client.js
+++ client.js
@@ -5,31 +5,65 @@ var peerApi   = require('./lib/rpc')
 var pull      = require('pull-stream')
 var toAddress = require('./lib/util').toAddress
 
+var handshake = require('secret-handshake')
+var ssbCap    = require('./lib/ssb-cap')
+
 // createClient  to a peer as a client
 // - `address.host`: string, hostname of the target
 // - `address.port`: number, port of the target
+// - `address.key` : Buffer[32], public key of the target
+
+var isBuffer = Buffer.isBuffer
+
+function toBuffer(base64) {
+  return new Buffer(base64.substring(0, base64.indexOf('.')), 'base64')
+}
+
+function toSodiumKeys (keys) {
+  return {
+    publicKey: toBuffer(keys.public),
+    secretKey: toBuffer(keys.private)
+  }
+}
 
 function isFunction (f) {
   return 'function' === typeof f
 }
-module.exports = function (address, manf, cb) {
+
+module.exports = function (keys, manf) {
+  if(!keys || (!keys.private || !keys.public))
+    throw new Error('must have public/private keys')
+
+  var createClientStream = handshake.client(toSodiumKeys(keys), ssbCap)
   manf = manf || manifest
+  return function (address, cb) {
+    var publicKey = toBuffer(address.key)
+    if(!isBuffer(publicKey))
+      return cb(new Error('*must* have remote public key'))
 
-  if(isFunction(manf) || !manf)
-    cb = manf, manf = manifest
+    var stream = net.connect(toAddress(address))
 
-  var stream = net.connect(toAddress(address), cb)
-  var rpc = peerApi.clientApi(manf, {auth: 'async'}, {
-    auth: function (req, cb) {
-      cb(null, {type: 'server'})
-    }
-  })
-              .permissions({allow: ['auth'], deny: null})
-  rpc.client = true
-  //match the server's way of tracking rpc direction.
-  //I don't know if we'll need this, but for consistency.
-  rpc.outgoing = true; rpc.incoming = false
-  pull(stream, rpc.createStream(), stream)
-  return rpc
+    pull(
+      stream,
+      createClientStream(publicKey, function (err, secure) {
+        if(err) return cb(err)
+
+        console.log('SECURE', secure.remote.toString('hex'))
+        var rpc = peerApi.clientApi(manf, {auth: 'async'}, {
+          auth: function (req, cb) {
+            cb(null, {type: 'server'})
+          }
+        })
+        .permissions({allow: ['auth'], deny: null})
+        rpc.client = true
+        //match the server's way of tracking rpc direction.
+        //I don't know if we'll need this, but for consistency.
+        rpc.outgoing = true; rpc.incoming = false
+        pull(secure, rpc.createStream(), secure)
+        cb(null, rpc)
+      }),
+      stream
+    )
+  }
 }
 
