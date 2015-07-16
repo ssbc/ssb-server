b5649cecd28d63f0c3fdd7646c6501996c0688bd
Dominic Tarr <dominic.tarr@gmail.com>
2015-07-16 01:04:35 +0000

    more test coverage for blocking

--- test/block.js
+++ test/block.js
@@ -17,26 +17,34 @@ var toAddress = require('../lib/util').toAddress
 // 2. alice never tries to connect to bob. (removed from peers)
 // 3. carol will not give bob any, she will not give him any data from alice.
 
-tape('alice blocks bob, and bob cannot connect to alice', function (t) {
 
-  var dbA = createDB('test-block-alice', {
-      port: 45451, host: 'localhost', timeout: 1400,
-    })
-    .use(replicate).use(friends) //but not gossip, yet
-
-  var alice = dbA.feed
-
-  var dbB = createDB('test-block-bob', {
-      port: 45452, host: 'localhost', timeout: 600,
-    })
+var dbA = createDB('test-block-alice', {
+    port: 45451, host: 'localhost', timeout: 1400,
+  })
   .use(replicate).use(friends) //but not gossip, yet
 
-  var bob = dbB.feed
+
+var dbB = createDB('test-block-bob', {
+    port: 45452, host: 'localhost', timeout: 600,
+  })
+  .use(replicate).use(friends) //but not gossip, yet
+
+var dbC = createDB('test-block-carol', {
+    port: 45453, host: 'localhost', timeout: 600,
+  })
+  .use(replicate).use(friends) //but not gossip, yet
+
+var alice = dbA.feed
+var carol = dbC.feed
+var bob = dbB.feed
+
+tape('alice blocks bob, and bob cannot connect to alice', function (t) {
 
   //in the beginning alice and bob follow each other
   cont.para([
     alice.add('contact', {contact: {feed: bob.id},   following: true}),
-    bob  .add('contact', {contact: {feed: alice.id}, following: true})
+    bob  .add('contact', {contact: {feed: alice.id}, following: true}),
+    carol.add('contact', {contact: {feed: alice.id}, following: true})
   ]) (function (err) {
     if(err) throw err
 
@@ -67,36 +75,66 @@ tape('alice blocks bob, and bob cannot connect to alice', function (t) {
       if(--n) return
       rpc.close();
       aliceCancel(); bobCancel()
+      console.log('ALICE BLOCKS BOB', {
+        source: alice.id, dest: bob.id
+      })
       alice.add({type: 'contact', contact: {feed: bob.id}, flag: true})
       (function (err) {
+        if(err) throw err
+        pull(
+          dbA.ssb.links({
+            source: alice.id,
+            dest: bob.id,
+            type: 'feed',
+            rel: 'contact',
+            values: true
+          }),
+          pull.filter(function (op) {
+            return op.value.content.flag != null
+          }),
+          pull.collect(function (err, ary) {
+            if(err) throw err
+            t.ok(flagged = ary.pop().value.content.flag, 'alice did block bob')
 
-      pull(
-        dbA.ssb.links({
-          source: alice.id,
-          //
-          rel: 'contact',
-          values: true
-        }),
-        pull.filter(function (op) {
-          return op.value.content.flag != null
-        }),
-        pull.collect(function (err, ary) {
-          if(err) throw err
-          flagged = ary.pop()
-          if(flagged && flagged.value.content.flag)
+            //since bob is blocked, he should not be able to connect
+            dbB.connect(dbA.getAddress(), function (err) {
+              t.ok(err, 'bob is blocked, should fail to connect to alice')
 
-          dbA.close(); dbB.close()
-          t.end()
-        })
-      )
-//        dbB.connect(dbA.getAddress(), function (err) {
-//          //since bob is blocked, he should not be able to connect
-//          t.ok(err)
-//          dbA.close(); dbB.close()
-//          t.end()
-//        })
+              //but carol, should, because she is not blocked.
+              dbC.connect(dbA.getAddress(), function (err) {
+                t.notOk(err)
+                t.end()
+              })
+            })
+
+          })
+        )
       })
     }
   })
+})
 
+tape('carol does not let bob replicate with alice', function (t) {
+  return t.end()
+  //first, carol should have already replicated with alice.
+
+  //emits this event when did not allow bob to get this data.
+  dbC.on('replicate:blocked', function (data) {
+
+  })
+
+  dbB.on('replicate:finish', function (vclock) {
+    console.log('BOB REPLICATED FROM CAROL')
+    console.log(vclock)
+  })
+
+  dbB.connect(dbC.getAddress(), function(err) {
+
+  })
+
+})
+
+tape('cleanup!', function (t) {
+  dbA.close(); dbB.close(); dbC.close()
+  t.end()
 })
