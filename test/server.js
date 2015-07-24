840a4cf9774034fbae4363515dd6ca3f785a8c97
Dominic Tarr <dominic.tarr@gmail.com>
2015-07-24 22:39:51 +0100

    update test/server

--- test/server.js
+++ test/server.js
@@ -2,56 +2,69 @@
 var cont      = require('cont')
 var deepEqual = require('deep-equal')
 var tape      = require('tape')
+var ssbKeys   = require('ssb-keys')
+
+var toAddr    = require('../lib/util').toAddress
 
 // create 3 servers
 // give them all pub servers (on localhost)
 // and get them to follow each other...
-var replicate = require('../plugins/replicate')
-var gossip    = require('../plugins/gossip')
-var friends   = require('../plugins/friends')
-var logging   = require('../plugins/logging')
+
+var createSbot = require('../core')
+  .use(require('../plugins/friends'))
+  .use(require('../plugins/replicate'))
+  .use(require('../plugins/gossip'))
+  .use(require('../plugins/logging'))
 
 tape('replicate between 3 peers', function (t) {
 
   var u = require('./util')
+  var alice, bob, carol
+  var dbA = createSbot({
+    temp: 'server-alice',
+    port: 45451, timeout: 1400,
+    keys: alice = ssbKeys.generate(),
+  })
+  var dbB = createSbot({
+    temp: 'server-bob',
+    port: 45452, timeout: 1400,
+    keys: bob = ssbKeys.generate(),
+    seeds: [dbA.getAddress()]
+  })
+  var dbC = createSbot({
+    temp: 'server-carol',
+    port: 45453, timeout: 1400,
+    keys: carol = ssbKeys.generate(),
+    seeds: [dbA.getAddress()]
+  })
 
-  var dbA = u.createDB('test-alice', {
-      port: 45451, host: 'localhost', timeout: 1400,
-    })
-
-  var alice = dbA.feed
-
-  var seed_alice = {port: 45451, host: 'localhost', key: alice.keys.public}
-
-  var dbB = u.createDB('test-bob', {
-      port: 45452, host: 'localhost', timeout: 600,
-      seeds: [seed_alice]
-    })
-  var bob = dbB.feed
-  var seed_bob = {port: 45452, host: 'localhost', key: bob.keys.public}
-
-  var dbC = u.createDB('test-carol', {
-      port: 45453, host: 'localhost', timeout: 2000,
-      seeds: [seed_alice]
-    })
-  var carol = dbC.feed
-  var seed_carol = {port: 45453, host: 'localhost', key: carol.keys.public}
-
+  var apub = cont(dbA.publish)
+  var bpub = cont(dbB.publish)
+  var cpub = cont(dbC.publish)
 
   cont.para([
-    alice.add('pub', {address: seed_alice}),
-    bob  .add('pub', {address: seed_bob}),
-    carol.add('pub', {address: seed_carol}),
+    apub({type: 'pub', address: toAddr(dbA.getAddress())}),
+    bpub({type: 'pub', address: toAddr(dbB.getAddress())}),
+    cpub({type: 'pub', address: toAddr(dbC.getAddress())}),
 
-    alice.add('contact', {contact: {feed: bob.id},   following: true}),
-    alice.add('contact', {contact: {feed: carol.id}, following: true}),
+    apub({type: 'contact',
+      contact: {feed: bob.id}, following: true
+    }),
+    apub({type: 'contact',
+      contact: {feed: carol.id}, following: true
+    }),
 
-    bob  .add('contact', {contact: {feed: alice.id}, following: true}),
-    bob  .add('contact', {contact: {feed: carol.id}, following: true}),
+    bpub({type: 'contact',
+      contact: {feed: alice.id}, following: true
+    }),
+    bpub({type: 'contact', content: {
+      contact: {feed: carol.id}, following: true}
+    }),
 
-    carol.add('contact', {contact: {feed: alice.id}, following: true}),
-    carol.add('contact', {contact: {feed: bob.id},   following: true})
-  ]) (function () {
+    cpub({type: 'contact', contact: {feed: alice.id}, following: true}),
+    cpub({type: 'contact', contact: {feed: bob.id},   following: true})
+  ]) (function (err, ary) {
+    if(err) throw err
 
     //TODO: detect when everything has been replicated
     //and end the test.
@@ -71,25 +84,17 @@ tape('replicate between 3 peers', function (t) {
     }
 
     var serverA = check(dbA, 'ALICE')
-      .use(replicate).use(gossip).use(friends)
-
     var serverB = check(dbB, 'BOB')
-      .use(replicate).use(gossip).use(friends)
-
     var serverC = check(dbC, 'CAROL')
-      .use(replicate).use(gossip).use(friends)
 
     var n = 2
 
     function done () {
       if(--n) return
-      serverA.close()
-      serverB.close()
-      serverC.close()
+      dbA.close(true); dbB.close(true); dbC.close(true)
       t.ok(true)
       t.end()
     }
-
   })
 })
 
