0c7eba31b42dc162862c768674710ce99f4f26d7
Dominic Tarr <dominic.tarr@gmail.com>
2015-07-25 18:35:33 +0100

    update block tests

--- test/block.js
+++ test/block.js
@@ -1,14 +1,14 @@
 var cont = require('cont')
 var tape = require('tape')
 var pull = require('pull-stream')
+var ssbKeys = require('ssb-keys')
 
-var replicate = require('../plugins/replicate')
-var friends   = require('../plugins/friends')
-var block     = require('../plugins/block')
+var createSbot = require('../core')
+  .use(require('../plugins/friends'))
+  .use(require('../plugins/block'))
+  .use(require('../plugins/replicate'))
 
-var createDB = require('./util').createDB
 var toAddress = require('../lib/util').toAddress
-
 
 // alice, bob, and carol all follow each other,
 // but then bob offends alice, and she blocks him.
@@ -18,68 +18,65 @@ var toAddress = require('../lib/util').toAddress
 // 2. alice never tries to connect to bob. (removed from peers)
 // 3. carol will not give bob any, she will not give him any data from alice.
 
+var alice = createSbot({
+  temp:'test-block-alice', timeout: 1400,
+  keys:ssbKeys.generate()
+})
 
-var dbA = createDB('test-block-alice', {
-    port: 45451, host: 'localhost', timeout: 1400,
-  })
-  .use(replicate).use(friends).use(block) //but not gossip, yet
+var bob = createSbot({
+  temp: 'test-block-bob', timeout: 600,
+  keys:ssbKeys.generate()
+})
 
-
-var dbB = createDB('test-block-bob', {
-    port: 45452, host: 'localhost', timeout: 600,
-  })
-  .use(replicate).use(friends).use(block) //but not gossip, yet
-
-var dbC = createDB('test-block-carol', {
-    port: 45453, host: 'localhost', timeout: 600,
-  })
-  .use(replicate).use(friends).use(block) //but not gossip, yet
-
-var alice = dbA.feed
-var carol = dbC.feed
-var bob = dbB.feed
+var carol = createSbot({
+  temp: 'test-block-carol', timeout: 600,
+  keys:ssbKeys.generate()
+})
 
 tape('alice blocks bob, and bob cannot connect to alice', function (t) {
 
   //in the beginning alice and bob follow each other
   cont.para([
-    alice.add('contact', {contact: {feed: bob.id},   following: true}),
-    bob  .add('contact', {contact: {feed: alice.id}, following: true}),
-    carol.add('contact', {contact: {feed: alice.id}, following: true})
+    cont(alice.publish)({type: 'contact', contact: {feed: bob.id},   following: true}),
+    cont(bob  .publish)({type: 'contact', contact: {feed: alice.id}, following: true}),
+    cont(carol.publish)({type: 'contact', contact: {feed: alice.id}, following: true})
   ]) (function (err) {
     if(err) throw err
 
     var n = 3, rpc
 
-    dbB.connect(dbA.getAddress(), function (err, _rpc) {
+    bob.connect(alice.getAddress(), function (err, _rpc) {
       if(err) throw err
       //replication will begin immediately.
       rpc = _rpc
       next()
     })
 
-    var bobCancel = dbB.ssb.post(function (op) {
+    //get the next messages that are replicated to alice and bob,
+    //and check that these are the correct follow messages.
+    var bobCancel = bob.post(function (op) {
       //should be the alice's follow(bob) message.
-      console.log('BOB RECEIVED', op)
+      t.equal(op.value.author, alice.id)
       t.equal(op.value.content.contact.feed, bob.id)
       next()
     })
 
-    var aliceCancel = dbA.ssb.post(function (op) {
+    var aliceCancel = alice.post(function (op) {
       //should be the bob's follow(alice) message.
-      console.log(op)
+      t.equal(op.value.author, bob.id)
       t.equal(op.value.content.contact.feed, alice.id)
       next()
     })
 
     function next () {
       if(--n) return
+
       rpc.close(true, function () {
         aliceCancel(); bobCancel()
         console.log('ALICE BLOCKS BOB', {
           source: alice.id, dest: bob.id
         })
-        alice.add({
+        alice.publish({
           type: 'contact',
           contact: {feed: bob.id},
           flagged: true
@@ -87,10 +84,10 @@ tape('alice blocks bob, and bob cannot connect to alice', function (t) {
         (function (err) {
           if(err) throw err
 
-          t.ok(dbA.friends.get({source: alice.id, dest: bob.id, graph: 'flag'}))
+          t.ok(alice.friends.get({source: alice.id, dest: bob.id, graph: 'flag'}))
 
           pull(
-            dbA.ssb.links({
+            alice.links({
               source: alice.id,
               dest: bob.id,
               type: 'feed',
@@ -102,16 +99,17 @@ tape('alice blocks bob, and bob cannot connect to alice', function (t) {
             }),
             pull.collect(function (err, ary) {
               if(err) throw err
+              console.log(ary)
               t.ok(flagged = ary.pop().value.content.flagged, 'alice did block bob')
 
               //since bob is blocked, he should not be able to connect
-              dbB.connect(dbA.getAddress(), function (err, rpc) {
+              bob.connect(alice.getAddress(), function (err, rpc) {
                 t.ok(err, 'bob is blocked, should fail to connect to alice')
                 //but carol, should, because she is not blocked.
-                dbC.connect(dbA.getAddress(), function (err) {
+                carol.connect(alice.getAddress(), function (err) {
                   t.notOk(err)
                 })
-                dbC.once('replicate:finish', function (vclock) {
+                carol.once('replicate:finish', function (vclock) {
                   t.equal(vclock[alice.id], 2)
                   //in next test, bob connects to carol...
                   t.end()
@@ -126,16 +124,15 @@ tape('alice blocks bob, and bob cannot connect to alice', function (t) {
 })
 
 tape('carol does not let bob replicate with alice', function (t) {
-//  return t.end()
   //first, carol should have already replicated with alice.
   //emits this event when did not allow bob to get this data.
-  dbB.once('replicate:finish', function (vclock) {
+  bob.once('replicate:finish', function (vclock) {
     console.log('BOB REPLICATED FROM CAROL')
     t.equal(vclock[alice.id], 1)
     console.log('ALICE:', alice.id)
     t.end()
   })
-  dbB.connect(dbC.getAddress(), function(err) {
+  bob.connect(carol.getAddress(), function(err) {
     if(err) throw err
   })
 })
@@ -147,6 +144,6 @@ tape('carol does not let bob replicate with alice', function (t) {
 //     when he is already connected to alice's friend.
 
 tape('cleanup!', function (t) {
-  dbA.close(); dbB.close(); dbC.close()
+  alice.close(true); bob.close(true); carol.close(true)
   t.end()
 })
