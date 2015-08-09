26d6ec71f99ecaaef77c99052b6e233535296f50
Dominic Tarr <dominic.tarr@gmail.com>
2015-08-09 23:10:50 +0200

    plugins: make local work right

--- plugins/local.js
+++ plugins/local.js
@@ -7,33 +7,17 @@ function isFunction (f) {
 
 module.exports = {
   name: 'local',
-  version: '1.0.0',
-  manifest: {
-    get: 'sync'
-  },
-  init: function (server) {
+  version: '2.0.0',
+  init: function (sbot, config) {
 
-    var local = broadcast(server.config.port)
-
-    var id = server.feed.id
-    var peers = {}
-    function toArray (o) {
-      return Object.keys(o).map(function (k) { return o[k] })
-    }
+    var local = broadcast(config.port)
 
     local.on('data', function (buf) {
       if(buf.loopback) return
 
-      var data = JSON.parse(buf.toString())
-      data.host = buf.address
-      var ts = Date.now()
-      data.ts = ts
-      var isNew = (data.id in peers)
-      peers[data.id] = data
-
-      if (!isNew)
-        server.emit('log:info', ['local', null, 'discovered', data])
-      server.emit('local', data)
+      var data = buf.toString()
+      console.log(data)
+      sbot.gossip.add(data, 'local')
     })
 
     setInterval(function () {
@@ -43,27 +27,9 @@ module.exports = {
       // (which means they can update their peer table)
       // Oh if this includes your local address,
       // then it becomes unforgeable.
-      local.write(JSON.stringify({
-        id: id,
-        port: server.config.port
-      }))
-
-      // clean out expired entries
-      var ts = Date.now()
-      for(var k in peers) {
-        if(peers[k].ts + 3000 < ts)
-          delete peers[k]
-      }
-      server.localPeers = toArray(peers)
+      console.log(server.getAddress())
+      local.write(server.getAddress())
     }, 1000)
-
-    return {
-      get: function () {
-        return toArray(peers)
-      }
-    }
-
   }
-
 }
 
