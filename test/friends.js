828a75a1f21f4fb4f899b6d190eef5f816b1ecf1
Dominic Tarr <dominic.tarr@gmail.com>
2015-10-25 12:22:29 -0700

    test: better coverage for plugins/friends.js

--- test/friends.js
+++ test/friends.js
@@ -2,12 +2,15 @@ var ssbKeys = require('ssb-keys')
 var cont    = require('cont')
 var tape    = require('tape')
 var u       = require('./util')
+var pull    = require('pull-stream')
+
 // create 3 feeds
 // add some of friend edges (follow, flag)
 // make sure the friends plugin analyzes correctly
 
 var createSbot = require('../')
   .use(require('../plugins/friends'))
+
 
 tape('construct and analyze graph', function (t) {
 
@@ -23,67 +26,88 @@ tape('construct and analyze graph', function (t) {
   var bob = sbot.createFeed()
   var carol = sbot.createFeed()
 
-  cont.para([
-    alice.add({
-      type: 'contact', contact: bob.id,
-      following: true,
-      flagged: { reason: 'foo' }
-    }),
-    alice.add(u.follow(carol.id)),
-    bob.add(u.follow(alice.id)),
-    bob.add({
-      type: 'contact', contact: carol.id,
-      following: false, flagged: true
-    }),
-    carol.add(u.follow(alice.id))
-  ]) (function (err, results) {
-    if(err) throw err
+  t.test('add friends, and retrive all friends for a peer', function (t) {
 
     cont.para([
-      cont(sbot.friends.all)(),
-      cont(sbot.friends.all)('follow'),
-      cont(sbot.friends.all)('flag'),
-
-      cont(sbot.friends.hops)(alice.id),
-      cont(sbot.friends.hops)(alice.id, 'follow'),
-      cont(sbot.friends.hops)(alice.id, 'flag'),
-
-      cont(sbot.friends.hops)(bob.id, 'follow'),
-      cont(sbot.friends.hops)(bob.id, 'flag'),
-
-      cont(sbot.friends.hops)(carol.id, 'follow'),
-      cont(sbot.friends.hops)(carol.id, 'flag')
-    ], function (err, results) {
+      alice.add({
+        type: 'contact', contact: bob.id,
+        following: true,
+        flagged: { reason: 'foo' }
+      }),
+      alice.add(u.follow(carol.id)),
+      bob.add(u.follow(alice.id)),
+      bob.add({
+        type: 'contact', contact: carol.id,
+        following: false, flagged: true
+      }),
+      carol.add(u.follow(alice.id))
+    ]) (function (err, results) {
       if(err) throw err
 
-      var aliasMap = {}
-      aliasMap[alice.id] = 'alice'
-      aliasMap[bob.id]   = 'bob'
-      aliasMap[carol.id] = 'carol'
+      cont.para([
+        cont(sbot.friends.all)(),
+        cont(sbot.friends.all)('follow'),
+        cont(sbot.friends.all)('flag'),
 
-      a = toAliases(aliasMap)
+        cont(sbot.friends.hops)(alice.id),
+        cont(sbot.friends.hops)(alice.id, 'follow'),
+        cont(sbot.friends.hops)(alice.id, 'flag'),
 
-      results = results.map(a)
-      var i = 0
+        cont(sbot.friends.hops)(bob.id, 'follow'),
+        cont(sbot.friends.hops)(bob.id, 'flag'),
 
-      t.deepEqual(results[i++], { alice: { bob: true, carol: true }, bob: { alice: true }, carol: { alice: true } })
-      t.deepEqual(results[i++], { alice: { bob: true, carol: true }, bob: { alice: true }, carol: { alice: true } })
-      t.deepEqual(results[i++], { alice: { bob: { reason: 'foo' } }, bob: { carol: true }, carol: {} })
+        cont(sbot.friends.hops)(carol.id, 'follow'),
+        cont(sbot.friends.hops)(carol.id, 'flag')
+      ], function (err, results) {
+        if(err) throw err
 
-      t.deepEqual(results[i++], { alice: 0, bob: 1, carol: 1 })
-      t.deepEqual(results[i++], { alice: 0, bob: 1, carol: 1 })
-      t.deepEqual(results[i++], { alice: 0, bob: 1, carol: 2 })
+        var aliasMap = {}
+        aliasMap[alice.id] = 'alice'
+        aliasMap[bob.id]   = 'bob'
+        aliasMap[carol.id] = 'carol'
 
-      t.deepEqual(results[i++], { bob: 0, alice: 1, carol: 2 })
-      t.deepEqual(results[i++], { bob: 0, carol: 1 })
+        a = toAliases(aliasMap)
 
-      t.deepEqual(results[i++], { carol: 0, alice: 1, bob: 2 })
-      t.deepEqual(results[i++], { carol: 0 })
+        results = results.map(a)
+        var i = 0
 
-      t.end()
-      sbot.close()
+        t.deepEqual(results[i++], { alice: { bob: true, carol: true }, bob: { alice: true }, carol: { alice: true } })
+        t.deepEqual(results[i++], { alice: { bob: true, carol: true }, bob: { alice: true }, carol: { alice: true } })
+        t.deepEqual(results[i++], { alice: { bob: { reason: 'foo' } }, bob: { carol: true }, carol: {} })
+
+        t.deepEqual(results[i++], { alice: 0, bob: 1, carol: 1 })
+        t.deepEqual(results[i++], { alice: 0, bob: 1, carol: 1 })
+        t.deepEqual(results[i++], { alice: 0, bob: 1, carol: 2 })
+
+        t.deepEqual(results[i++], { bob: 0, alice: 1, carol: 2 })
+        t.deepEqual(results[i++], { bob: 0, carol: 1 })
+
+        t.deepEqual(results[i++], { carol: 0, alice: 1, bob: 2 })
+        t.deepEqual(results[i++], { carol: 0 })
+
+        t.end()
+      })
     })
   })
+
+  t.test('creatFriendStream', function () {
+    pull(
+      sbot.friends.createFriendStream(),
+      pull.collect(function (err, ary) {
+        t.notOk(err)
+        console.log(ary)
+        t.equal(ary.length, 3)
+        t.deepEqual(ary.sort(), [alice.id, bob.id, carol.id].sort())
+        t.end()
+      })
+    )
+  })
+
+  t.test('cleanup', function (t) {
+    sbot.close()
+    t.end()
+  })
+
 })
 
 tape('correctly delete edges', function (t) {
@@ -100,68 +124,149 @@ tape('correctly delete edges', function (t) {
   var bob   = sbot.createFeed()
   var carol = sbot.createFeed()
 
-  cont.para([
-    alice.add({
-      type:'contact', contact:bob.id,
-      following: true, flagged: true
-    }),
-    alice.add(u.follow(carol.id)),
-    bob.add(u.follow(alice.id)),
-    bob.add({
-      type: 'contact', contact: carol.id,
-      following: false, flagged: { reason: 'foo' }
-    }),
-    carol.add(u.follow(alice.id)),
-    alice.add({
-      type:'contact', contact: carol.id,
-      following: false,  flagged: true
-    }),
-    alice.add({
-      type:'contact', contact: bob.id,
-      following: true,  flagged: false
-    }),
-    bob.add(u.unfollow(carol.id))
-  ]) (function () {
+  t.test('add and delete', function (t) {
 
     cont.para([
-      cont(sbot.friends.all)('follow'),
-      cont(sbot.friends.all)('flag'),
+      alice.add({
+        type:'contact', contact:bob.id,
+        following: true, flagged: true
+      }),
+      alice.add(u.follow(carol.id)),
+      bob.add(u.follow(alice.id)),
+      bob.add({
+        type: 'contact', contact: carol.id,
+        following: false, flagged: { reason: 'foo' }
+      }),
+      carol.add(u.follow(alice.id)),
+      alice.add({
+        type:'contact', contact: carol.id,
+        following: false,  flagged: true
+      }),
+      alice.add({
+        type:'contact', contact: bob.id,
+        following: true,  flagged: false
+      }),
+      bob.add(u.unfollow(carol.id))
+    ]) (function () {
 
-      cont(sbot.friends.hops)(alice.id, 'follow'),
-      cont(sbot.friends.hops)(alice.id, 'flag'),
+      cont.para([
+        cont(sbot.friends.all)('follow'),
+        cont(sbot.friends.all)('flag'),
 
-      cont(sbot.friends.hops)(bob.id, 'follow'),
-      cont(sbot.friends.hops)(bob.id, 'flag'),
+        cont(sbot.friends.hops)(alice.id, 'follow'),
+        cont(sbot.friends.hops)(alice.id, 'flag'),
 
-      cont(sbot.friends.hops)(carol.id, 'follow'),
-      cont(sbot.friends.hops)(carol.id, 'flag')
-    ], function (err, results) {
+        cont(sbot.friends.hops)(bob.id, 'follow'),
+        cont(sbot.friends.hops)(bob.id, 'flag'),
 
-      var aliasMap = {}
-      aliasMap[alice.id] = 'alice'
-      aliasMap[bob.id]   = 'bob'
-      aliasMap[carol.id] = 'carol'
-      a = toAliases(aliasMap)
+        cont(sbot.friends.hops)(carol.id, 'follow'),
+        cont(sbot.friends.hops)(carol.id, 'flag')
+      ], function (err, results) {
 
-      results = results.map(a)
-      var i = 0
+        var aliasMap = {}
+        aliasMap[alice.id] = 'alice'
+        aliasMap[bob.id]   = 'bob'
+        aliasMap[carol.id] = 'carol'
+        a = toAliases(aliasMap)
 
-      t.deepEqual(results[i++], { alice: { bob: true }, bob: { alice: true }, carol: { alice: true } })
-      t.deepEqual(results[i++],  { alice: { carol: true }, bob: { carol: { reason: 'foo' }}, carol: {} })
+        results = results.map(a)
+        var i = 0
 
-      t.deepEqual(results[i++], { alice: 0, bob: 1 })
-      t.deepEqual(results[i++], { alice: 0, carol: 1 })
+        t.deepEqual(results[i++], { alice: { bob: true }, bob: { alice: true }, carol: { alice: true } })
+        t.deepEqual(results[i++],  { alice: { carol: true }, bob: { carol: { reason: 'foo' }}, carol: {} })
 
-      t.deepEqual(results[i++], { bob: 0, alice: 1 })
-      t.deepEqual(results[i++], { bob: 0, carol: 1 })
+        t.deepEqual(results[i++], { alice: 0, bob: 1 })
+        t.deepEqual(results[i++], { alice: 0, carol: 1 })
 
-      t.deepEqual(results[i++], { carol: 0, alice: 1, bob: 2 })
-      t.deepEqual(results[i++], { carol: 0 })
+        t.deepEqual(results[i++], { bob: 0, alice: 1 })
+        t.deepEqual(results[i++], { bob: 0, carol: 1 })
 
-      t.end()
-      sbot.close()
+        t.deepEqual(results[i++], { carol: 0, alice: 1, bob: 2 })
+        t.deepEqual(results[i++], { carol: 0 })
+
+        t.end()
+      })
     })
   })
+
+  t.test('createFriendStream after delete', function (t) {
+    pull(
+      sbot.friends.createFriendStream(),
+      pull.collect(function (err, ary) {
+        t.notOk(err)
+        console.log(ary)
+        t.equal(ary.length, 2)
+        t.deepEqual(ary.sort(), [alice.id, bob.id].sort())
+        t.end()
+      })
+    )
+  })
+
+  t.test('cleanup', function (t) {
+    sbot.close()
+    t.end()
+  })
+
+})
+
+tape('indirect friends', function (t) {
+
+  var aliceKeys = ssbKeys.generate()
+
+  var sbot = createSbot({
+      temp:'test-friends3',
+      port: 45452, host: 'localhost', timeout: 1000,
+      keys: aliceKeys
+    })
+
+  var alice = sbot.createFeed(aliceKeys)
+  var bob   = sbot.createFeed()
+  var carol = sbot.createFeed()
+  var dan   = sbot.createFeed()
+
+  t.test('chain of friends', function (t) {
+    cont.para([
+      alice.add(u.follow(bob.id)),
+      bob.add(u.follow(carol.id)),
+      carol.add(u.follow(dan.id))
+    ]) (function (err, results) {
+      if(err) throw err
+
+      sbot.friends.hops({hops: 3}, function (err, all) {
+        if(err) throw err
+        console.log(all)
+        var o = {}
+
+        o[alice.id] = 0
+        o[bob.id]   = 1
+        o[carol.id] = 2
+        o[dan.id]   = 3
+
+        t.deepEqual(all, o)
+
+        t.end()
+      })
+    })
+  })
+
+  t.test('createFriendStream on long chain', function (t) {
+
+    pull(
+      sbot.friends.createFriendStream(),
+      pull.collect(function (err, ary) {
+        if(err) throw err
+        console.log(ary)
+        t.end()
+      })
+    )
+
+  })
+
+  t.test('cleanup', function (t) {
+    sbot.close()
+    t.end()
+  })
+
 })
 
 function toAliases(aliasMap) {
