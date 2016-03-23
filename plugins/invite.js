b59e95a0db24ce6bc69fd9271a61a4721a69d6e4
Paul Frazee <pfrazee@gmail.com>
2016-03-23 15:50:55 -0500

    if an invite code is surrounded in quotes, automatically remove them

--- plugins/invite.js
+++ plugins/invite.js
@@ -117,6 +117,10 @@ module.exports = {
         })
       }, 'object'),
       accept: valid.async(function (invite, cb) {
+        // remove surrounding quotes, if found
+        if (invite.charAt(0) === '"' && invite.charAt(invite.length - 1) === '"')
+          invite = invite.slice(1, -1)
+
         var parts = invite.split('~')
         var addr = toAddress(parts[0])
 
