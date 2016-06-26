ssbc/ssb-server

commit 11f01217c2da03b8d2f36ceba40ec1746b9560c1

oops, fix so you can actually start the server when not using --

bin.js
@@ -17,7 +17,7 @@ var cmdAliases   = require('./lib/cli-cmd-aliases')
 var argv = process.argv.slice(2)
 var i = argv.indexOf('--')
 var conf = argv.slice(i+1)
-argv = argv.slice(0, i)
+argv = ~i ? argv.slice(0) : argv
 
 var config = require('ssb-config/inject')(process.env.ssb_appname, minimist(conf))
 

commit 2876553101471d394bb89137e0fa97e2ee1bdfc8

Fix 11f01217 to make 0f96771a work again
Commit 11f01217 (oops, fix so you can actually start the server when not
using --, 2016-05-15) broke the change from commit 0f96771a (separate
argv by -- and use later part for configuration., 2016-05-14).

Fix this by adding back the i part of the argv.slice.

Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

bin.js
@@ -17,7 +17,7 @@ var cmdAliases   = require('./lib/cli-cmd-aliases')
 var argv = process.argv.slice(2)
 var i = argv.indexOf('--')
 var conf = argv.slice(i+1)
-argv = ~i ? argv.slice(0) : argv
+argv = ~i ? argv.slice(0, i) : argv
 
 var config = require('ssb-config/inject')(process.env.ssb_appname, minimist(conf))
 