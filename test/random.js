b3c01b8fda9e069ecb5d1a802fafc9c563c60ee1
Dominic Tarr <dominic.tarr@gmail.com>
2016-10-02 19:33:56 +1300

    better tests of replication performance

--- test/random.js
+++ test/random.js
@@ -2,11 +2,14 @@ var pull      = require('pull-stream')
 var paramap   = require('pull-paramap')
 var ssbKeys   = require('ssb-keys')
 var u         = require('./util')
+var ssbClient = require('ssb-client')
 
 var tape      = require('tape')
 
 var cats = require('cat-names')
 var dogs = require('dog-names')
+
+var generated = {}, F=100,N=10000
 
 //build a random network, with n members.
 function bar (prog) {
@@ -15,7 +18,7 @@ function bar (prog) {
   for(var i = 0; i < M; i++)
     s += i < M*r ? '*' : '.'
 
-  return s + ' '+prog.progress+'/'+prog.total+':'+prog.feeds
+  return s + ' '+prog.progress+'/'+prog.total+'  '//+':'+prog.feeds
 }
 
 function isNumber (n) {
@@ -27,10 +30,10 @@ var createSbot = require('../')
   .use(require('../plugins/replicate'))
   .use(require('../plugins/gossip'))
 
-function generateAnimals (sbot, feed, n, cb) {
+function generateAnimals (sbot, feed, f, n, cb) {
   var a = [feed]
 
-  while(n --> 0)
+  while(f --> 0)
     a.push(sbot.createFeed())
 
   console.log('generate NAMES')
@@ -50,14 +53,14 @@ function generateAnimals (sbot, feed, n, cb) {
       var posts = []
 
       pull(
-        pull.count(10000),
+        pull.count(n),
         paramap(function (n, cb) {
 
           var me = a[~~(Math.random()*a.length)]
           var r = Math.random()
 
           //one in 20 messages is a random follow
-          if(r < 0.3) {
+          if(r < 0.5) {
             var f = a[~~(Math.random()*a.length)]
             me.add(u.follow(f.id), cb)
           } else if(r < 0.6) {
@@ -81,7 +84,7 @@ function generateAnimals (sbot, feed, n, cb) {
             })
 
           }
-        }, 10),
+        }, 32),
         pull.drain(null, cb)
       )
 
@@ -91,13 +94,12 @@ function generateAnimals (sbot, feed, n, cb) {
 }
 
 function latest (sbot, cb) {
-  sbot.friends.hops({hops: 2}, function (err, keys) {
+  sbot.friends.hops({hops: 3}, function (err, keys) {
     if(err) return cb(err)
     var n = 0, map = {}
     for(var k in keys) (function (key) {
       n++
       sbot.latestSequence(key, function (err, value) {
-        console.log(value)
         map[key] = isNumber(value) ? value : value.sequence
         if(--n) return
         cb(null, map)
@@ -106,97 +108,172 @@ function latest (sbot, cb) {
   })
 }
 
-tape('replicate social network for animals', function (t) {
-
   var alice = ssbKeys.generate()
   var bob   = ssbKeys.generate()
 
-  var animalNetwork = createSbot({
-    temp: 'test-random-animals',
-    port: 45451, host: 'localhost', timeout: 20001,
-    replication: {hops: 3}, keys: alice
+
+
+var animalNetwork = createSbot({
+  temp: 'test-random-animals',
+  port: 45451, host: 'localhost', timeout: 20001,
+  replication: {hops: 3}, keys: alice
+})
+
+pull(
+  animalNetwork.replicate.changes(),
+  pull.drain(function (prog) {
+    prog.id = 'animal network'
+    console.log(prog)
   })
+)
 
-  if(!animalNetwork.friends)
-    throw new Error('missing frineds plugin')
-
-  generateAnimals(animalNetwork, {add: animalNetwork.publish}, 500, function (err) {
+tape('generate random network', function (t) {
+  var start = Date.now()
+  generateAnimals(animalNetwork, {add: animalNetwork.publish}, F, N, function (err) {
     if(err) throw err
     console.log('replicate GRAPH')
     var c = 0
-    latest(animalNetwork, function (err, latest) {
+    latest(animalNetwork, function (err, _generated) {
+      if(err) throw err
 
-      var seen = {}
-      var start = Date.now()
-      var animalFriends = createSbot({
-        temp: 'test-random-animals2',
-        port: 45452, host: 'localhost', timeout: 20001,
-        replication: {hops: 3},
-        progress: true,
-        seeds: [animalNetwork.getAddress()],
-        keys: bob
-      })
+      generated = _generated
+      var total = 0, feeds = 0
+      for(var k in generated) {
+        total += generated[k]
+        feeds ++
+      }
 
-      var progress = []
+      var time = (Date.now()-start)/1000
+      console.log('generated', total, 'messages in', time, 'at rate:', total/time)
+      console.log('over', feeds, 'feeds')
+      t.equal(total, N+1+F+1)
+      t.equal(feeds, F+1)
+      t.end()
+    })
+  })
+})
 
+tape('read all history streams', function (t) {
+  var opts = {
+    host: 'localhost', port: 45451,
+    key: alice.id,
+    manifest: animalNetwork.manifest()
+  }
+
+  var dump = createSbot({
+    temp: 'test-random-animals_dump',
+//    port: 45453, host: 'localhost', timeout: 20001,
+    keys: bob
+  })
+  var live = 0, listeners = 0
+  var since = {}
+
+  var h = 0
+
+  pull(
+    dump.createLogStream({live: true, keys: false}),
+    pull.drain(function (e) {
+      live ++
+    })
+  )
+
+//  setTimeout(function () {
+
+    ssbClient(bob, opts, function (err, sbot) {
+      if(err) throw err
+
+      var wants = {}, n = 0, c = 0, start = Date.now()
+
+      //TODO, test just dumping everything!
       pull(
-        animalFriends.replicate.changes(),
-        pull.drain(function (prog) {
-          progress.push(prog)
-          process.stdout.write(bar(prog))
-        })
-      )
-
-      animalFriends.once('rpc:connect', function (rpc) {
-        console.log('CONNECTION', Date.now(), c++)
-      })
-
-      if(!animalFriends.friends)
-        throw new Error('missing friends plugin')
-
-      animalFriends.publish({
-        type: 'contact',
-        contact: animalNetwork.id,
-        following: true
-      }, function (err, msg) {
-        if(err) throw err
-      })
-
-      pull(
-        animalFriends.createLogStream({live: true}),
-        pull.drain(function (data) {
-          if(data.sync) return
-          seen[data.value.author] = data.value.sequence
-          var total = 0, prog = 0
-          for(var k in latest) {
-            total += latest[k]
-            prog += (seen[k] || 0)
-          }
-
-          //console.log("REPLICATED", prog, total, Math.round(100*(prog/total)))
-          if(total === prog && total !== 0) {
-            var seconds = (Date.now() - start)/1000
-            t.equal(c, 1, 'counter is as expected')
-            t.equal(prog, total)
-
-            //WAIT FOR THE LAST PROGRESS UPDATE... (100ms)
-            setTimeout(function () {
-
-              t.ok(progress.length)
-              var last = progress.pop()
-              t.equal(last.total, total, 'last.total')
-              t.equal(last.progress, total, 'last.total')
-
-              console.log("DONE", seconds, c, total/seconds)
-              animalFriends.close(true)
-              animalNetwork.close(true)
+        sbot.createLogStream({keys: false}),
+        pull.through(function (n) {
+          c++
+        }),
+        dump.createWriteStream(function (err, data) {
+          if(err) throw err
+          var time = (Date.now() - start)/1000
+          console.log("dump all messages via createLogStream")
+          console.log('all histories dumped', c, 'messages in', time, 'at rate', c/time)
+          console.log('read back live:', live, 'over', h, 'histories', listeners, 'listeners')
+          pull(
+            dump.createLogStream(),
+            pull.collect(function (err, ary) { 
+              if(err) throw err
+              t.ok(ary.length, F+N+2)
+              sbot.close()
+              dump.close()
               t.end()
-
-            }, 200)
-          }
+            })
+          )
         })
       )
     })
+//  }, 1000)
+})
+
+tape('replicate social network for animals', function (t) {
+  //return t.end()
+  var c = 0
+  if(!animalNetwork.friends)
+    throw new Error('missing frineds plugin')
+
+  var start = Date.now()
+  var animalFriends = createSbot({
+    temp: 'test-random-animals2',
+    port: 45452, host: 'localhost', timeout: 20001,
+    replication: {hops: 3},
+    progress: true,
+    seeds: [animalNetwork.getAddress()],
+    keys: bob
   })
+  animalFriends.logging
+  var connections = 0
+
+  animalFriends.on('rpc:connect', function (rpc) {
+    connections++
+    console.log("CONNECT", c)
+    rpc.on('closed', function () {
+      console.log("DISCONNECT", -c)
+    })
+  })
+
+  pull(
+    animalFriends.replicate.changes(),
+    pull.drain(function (prog) {
+      prog.id = 'animal friends'
+      var target = F+N+3
+      console.log(prog, target)
+//      progress.push(prog)
+//      process.stdout.write(bar(prog))
+      if(prog.progress === target) {
+        console.log("DONE!!!!")
+        var time = (Date.now() - start) / 1000
+        console.log('replicated', target, 'messages in', time, 'at rate',target/time)
+        animalFriends.close(true)
+        t.end()
+      }
+    })
+  )
+
+  animalFriends.logging = true
+
+  if(!animalFriends.friends)
+    throw new Error('missing friends plugin')
+
+  animalFriends.publish({
+    type: 'contact',
+    contact: animalNetwork.id,
+    following: true
+  }, function (err, msg) {
+    if(err) throw err
+  })
+
+})
+
+
+tape('shutdown', function (t) {
+  animalNetwork.close(true)
+  t.end()
 })
 
