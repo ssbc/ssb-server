f01447184bc4318a21262c0bed6a7a726e6e3bfe
Dominic Tarr <dominic.tarr@gmail.com>
2016-10-02 19:44:07 +1300

    rewrite plugins/replicate.js for better perf

--- plugins/replicate.js
+++ plugins/replicate.js
@@ -1,3 +1,4 @@
+'use strict'
 var pull = require('pull-stream')
 var para = require('pull-paramap')
 var Notify = require('pull-notify')
@@ -9,11 +10,12 @@ var Observ = require('observ')
 var mdm = require('mdmanifest')
 var apidoc = require('../lib/apidocs').replicate
 
+var Pushable = require('pull-pushable')
+
 // compatibility function for old implementations of `latestSequence`
 function toSeq (s) {
   return 'number' === typeof s ? s : s.sequence
 }
-
 
 function last (a) { return a[a.length - 1] }
 
@@ -23,33 +25,37 @@ module.exports = {
   manifest: mdm.manifest(apidoc),
   //replicate: replicate,
   init: function (sbot, config) {
-
     var debounce = Debounce(200)
-
+    var listeners = {}
     var notify = Notify()
     var newPeer = Notify()
-
-    pull(notify.listen(), pull.log())
 
     var total=0, progress=0, start, count = 0, rate=0
     var to_send = {}
     var to_recv = {}
+    var feeds = 0
 
     debounce(function () {
       var _progress = progress, _total = total
       progress = 0; total = 0
       for(var k in to_send) progress += to_send[k]
-      for(var k in to_recv) total += to_recv[k]
-      if(_progress !== progress || _total !== total)
+
+      for(var k in to_recv)
+        if(to_send[k] !== null)
+          total += to_recv[k]
+
+      if(_progress !== progress || _total !== total) {
         notify({
-          total: _total, progress: _progress, rate: rate
-        })
+            id: sbot.id,
+            total: total, progress: progress, rate: rate,
+            feeds: feeds
+          })
+        }
     })
 
     pull(
       sbot.createLogStream({old: false, live: true, sync: false, keys: false}),
       pull.drain(function (e) {
-        console.log(e)
         //track writes per second, mainly used for developing initial sync.
         if(!start) start = Date.now()
         var time = (Date.now() - start)/1000
@@ -58,28 +64,44 @@ module.exports = {
           start = Date.now()
           count = 0
         }
+        var pushable = listeners[e.author]
+
+        if(pushable && pushable.sequence == e.sequence) {
+          pushable.sequence ++
+          pushable.forEach(function (p) {
+            p.push(e)
+          })
+        }
         count ++
         addPeer({id: e.author, sequence: e.sequence})
       })
     )
 
-
     //keep track of maximum requested value, per feed.
     sbot.createHistoryStream.hook(function (fn, args) {
       var upto = args[0] || {}
-      to_recv[upto.id] = Math.max(to_recv[upto.id] || 0, (upto.sequence || upto.seq) - 1)
+      var seq = upto.sequence || upto.seq
+      to_recv[upto.id] = Math.max(to_recv[upto.id] || 0, seq)
       if(this._emit) this._emit('call:createHistoryStream', args[0])
 
-      //compare request to in memory table, to avoid hitting the disk if possible.
-      if(!to_send[upto.id])
-        return pull.error(new Error('not known'))
-      //optimization: if we already know are already uptodate, do not actually hit the disk.
-      else if(to_send[upto.id] < upto.sequence)
-        return sbot.createHistoryStream({old: false, id: upto.id, sequence: upto.sequence})
-      else
-        return fn.apply(this, args)
-    })
+      debounce.set()
 
+      //handle creating lots of histor streams efficiently.
+      //maybe this could be optimized in map-filter-reduce queries instead?
+      if(to_send[upto.id] == null || (seq > to_send[upto.id])) {
+        upto.old = false
+        if(!upto.live) return pull.empty()
+        var pushable = listeners[upto.id] = listeners[upto.id] || []
+        var p = Pushable(function () {
+          var i = pushable.indexOf(p)
+          pushable.splice(i, 1)
+        })
+        pushable.push(p)
+        pushable.sequence = upto.sequence
+        return p
+      }
+      return fn(upto)
+    })
 
     // collect the IDs of feeds we want to request
     var opts = config.replication || {}
@@ -109,26 +131,25 @@ module.exports = {
     function addPeer (upto) {
       if(upto.sync) return
       if(!upto.id) return console.log('invalid', upto)
-      var isNew = false
-      if(to_send[upto.id] == null) isNew = true
 
-      to_send[upto.id] = Math.max(to_send[upto.id] || 0, upto.sequence || upto.seq || 0)
+      if(to_send[upto.id] == null) {
+        to_send[upto.id] = Math.max(to_send[upto.id] || 0, upto.sequence || upto.seq || 0)
+        newPeer({id: upto.id, sequence: to_send[upto.id] , type: 'new' })
+      } else
+        to_send[upto.id] = Math.max(to_send[upto.id] || 0, upto.sequence || upto.seq || 0)
 
-      if(isNew) newPeer({id: upto.id, sequence: to_send[upto.id]})
       debounce.set()
     }
 
+
     // create read-streams for the desired feeds
+    var S = false
     pull(
       sbot.friends.createFriendStream(opts),
       // filter out duplicates, and also keep track of what we expect to receive
-      pull.filter(function (s) {
-        var id = 'string' === typeof s ? s  : s.id
-        if(to_recv[id] == null) { to_recv[id] = 0; return true }
-      }),
       // lookup the latest sequence from each user
       para(function (data, cb) {
-        if(data.sync) return cb(null, data)
+        if(data.sync) return cb(null, S = data)
         var id = data.id || data
         sbot.latestSequence(id, function (err, seq) {
           cb(null, {
@@ -144,9 +165,8 @@ module.exports = {
       var ary = Object.keys(to_send).map(function (k) {
         return { id: k, sequence: to_send[k] }
       })
-
       if(opts.live)
-        return Cat([pull.values(ary), newPeer.listen()])
+        return Cat([pull.values(ary), pull.once({sync: true}), newPeer.listen()])
 
       return pull.values(ary)
     }
@@ -157,32 +177,29 @@ module.exports = {
 
       //check for local peers, or manual connections.
       localPeers()
-
       var drain
-
-      sbot.emit('log:info', ['replicate', rpc.id, 'start'])
       sbot.emit('replicate:start', rpc)
 
+      var SYNC = false
       pull(
-        upto({live: opts.live, sync: false}),
+        upto({live: opts.live}),
         drain = pull.drain(function (upto) {
+          if(upto.sync) return
 
+          feeds++
+          debounce.set()
           pull(
             rpc.createHistoryStream({
               id: upto.id,
-              seq: upto.sequence + 1,
+              seq: (upto.sequence || upto.seq) + 1,
               live: true,
               keys: false
             }),
-            pull.filter(function (e) {
-              //GUESSING. (maybe remove this?)
-              //incase we where receiving messages from two peers at once,
-              //and we have already seen this one, just skip it.
-              if(to_send[e.author] < e.sequence) return true
-            }),
             sbot.createWriteStream(function (err) {
               if(err) console.error(err.stack)
-              drain.abort()
+
+              feeds--
+              debounce.set()
             })
           )
 
@@ -195,36 +212,10 @@ module.exports = {
 
     return {
       changes: notify.listen,
-      upto: upto
+      upto: upto,
     }
   }
 }
-
-function summarizeProgress (progress) {
-  // count the number of feeds updated, and the number of new messages
-  var updatedFeeds = 0, newMessages = 0
-  for (var author in progress) {
-    if (progress[author] > 0) {
-      updatedFeeds++
-      newMessages += progress[author]
-    }
-  }
-  // no message if no updates
-  if (updatedFeeds === 0)
-    return false
-  return 'Feeds updated: '+updatedFeeds+', New messages: '+newMessages
-}
-
-
-
-
-
-
-
-
-
-
-
 
 
 
