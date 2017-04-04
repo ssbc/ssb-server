5ced0533fae7a19a11ef8a30f136689f475e3ae6
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-05 09:29:55 +1200

    send sync over history stream

--- plugins/replicate.js
+++ plugins/replicate.js
@@ -312,29 +312,22 @@ function createHistoryStreamWithSync (rpc, upto, onSync) {
   // know when it switches to live. Do it manually!
   var last = (upto.sequence || upto.seq || 0)
   var state = null
-  return pullNext(function () {
-    if (!state) {
-      state = 'old'
-      return pull(
-        rpc.createHistoryStream({
-          id: upto.id,
-          seq: last + 1,
-          live: false,
-          keys: false
-        }),
-        pull.through(msg => {
-          last = Math.max(last, msg.sequence)
-        })
-      )
-    } else if (state === 'old') {
-      state = 'sync'
-      onSync && onSync(true)
-      return rpc.createHistoryStream({
-        id: upto.id,
-        seq: last + 1,
-        live: true,
-        keys: false
-      })
-    }
-  })
+  return pull(
+    rpc.createHistoryStream({
+      id: upto.id,
+      seq: last + 1,
+      sync: false,
+      live: true,
+      keys: false
+    }),
+    pull.filter(msg => {
+      if(msg.sync) return false
+      last = Math.max(last, msg.sequence)
+      return true
+    })
+  )
 }
+
+
+
+
