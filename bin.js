095664dce83833ae080b31398a00ff67f2ba5dd7
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-05 15:19:41 +1200

    use ssb-ebt

--- bin.js
+++ bin.js
@@ -50,9 +50,10 @@ if (argv[0] == 'server') {
     .use(require('ssb-query'))
     .use(require('ssb-links'))
     .use(require('ssb-ws'))
+    .use(require('ssb-ebt'))
 
   // add third-party plugins
-  require('./plugins/plugins').loadUserPlugins(createSbot, config)
+//  require('./plugins/plugins').loadUserPlugins(createSbot, config)
 
   // start server
 
