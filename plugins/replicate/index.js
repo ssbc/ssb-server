74e50ad68eed6f53101d4642594609b28229e418
Dominic Tarr <dominic.tarr@gmail.com>
2017-06-01 10:55:51 +1200

    all replication policy moved to ssb-friends plugin

--- plugins/replicate/index.js
+++ plugins/replicate/index.js
@@ -12,36 +12,24 @@ module.exports = {
   manifest: mdm.manifest(apidoc),
   //replicate: replicate,
   init: function (sbot, config) {
-//    var change = Obv()
     var notify = Notify(), upto
     if(!config.replicate || config.replicate.legacy !== false) {
       var replicate = Legacy.call(this, sbot, notify, config)
 
-      var c = 0
-
-      //to change the replication policy, override this file
-      //and make different calls to replicate.request(id)
-      //to add things is easy, you just call replicate.request(id)
-      //calls to replicate.request(id) are ephemeral,
-      //and must be made each time run sbot.
-      pull(
-        sbot.friends.createFriendStream({live: true, meta: true}),
-        // filter out duplicates, and also keep track of what we expect to receive
-        // lookup the latest sequence from each user
-        // TODO: use paramap?
-        pull.drain(function (data) {
-          if(data.sync) return
-          if(data.hops >= 0)
-            replicate.request(data.id)
-          else
-            replicate.request(data.id, false)
-        })
-      )
+      // replication policy is set by calling
+      // sbot.replicate.request(id)
+      // or by cancelling replication
+      // sbot.replicate.request(id, false)
+      // this is currently performed from the ssb-friends plugin
 
       return replicate
     }
   }
 }
+
+
+
+
 
 
 
