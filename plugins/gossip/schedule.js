b509783e3e2cf452a4b43be0955795298068df92
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-05 21:05:46 +1300

    remove logging: skip gossip

--- plugins/gossip/schedule.js
+++ plugins/gossip/schedule.js
@@ -145,7 +145,6 @@ function (gossip, config, server) {
   function isCurrentlyDownloading () {
     // don't schedule gossip if currently downloading messages
     if (lastMessageAt && lastMessageAt > Date.now() - 500) {
-      console.log('skip gossip')
       return true
     }
   }
@@ -260,3 +259,4 @@ exports.isLocal = isLocal
 exports.isFriend = isFriend
 exports.isConnectedOrConnecting = isConnect
 exports.select = select
+
