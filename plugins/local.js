aac2d93811db9da34f33475e76d2000c07c837a2
Matt McKegg <matt@wetsand.co.nz>
2019-03-20 16:49:48 +1300

    pull in local plugin from ssb-server@14

--- plugins/local.js
+++ plugins/local.js
@@ -1,16 +1,13 @@
+// backport from sbot@14
+
 var broadcast = require('broadcast-stream')
 var ref = require('ssb-ref')
 // local plugin
-// broadcasts the address:port:pubkey triple of the sbot server
+// broadcasts the address:port:pubkey triple of the ssb server
 // on the LAN, using multicast UDP
 
-function isFunction (f) {
-  return 'function' === typeof f
-}
-
 function isEmpty (o) {
-  for(var k in o)
-    return false
+  for (var k in o) return false
   return true
 }
 
@@ -25,14 +22,15 @@ function isEmpty (o) {
 module.exports = {
   name: 'local',
   version: '2.0.0',
-  init: function init (sbot, config) {
-    if(config.gossip && config.gossip.local === false)
+  init: function init (ssbServer, config) {
+    if (config.gossip && config.gossip.local === false) {
       return {
         init: function () {
           delete this.init
-          init(sbot, config)
+          init(ssbServer, config)
         }
       }
+    }
 
     var local = broadcast(config.port)
     var addrs = {}
@@ -42,7 +40,7 @@ module.exports = {
     setInterval(function () {
       Object.keys(lastSeen).forEach((key) => {
         if (Date.now() - lastSeen[key] > 10e3) {
-          sbot.gossip.remove(addrs[key])
+          ssbServer.gossip.remove(addrs[key])
           delete lastSeen[key]
         }
       })
@@ -53,37 +51,39 @@ module.exports = {
       if (buf.loopback) return
       var data = buf.toString()
       var peer = ref.parseAddress(data)
-      if (peer && peer.key !== sbot.id) {
+      if (peer && peer.key !== ssbServer.id) {
         addrs[peer.key] = peer
         lastSeen[peer.key] = Date.now()
-        //note: add the raw data, not the parsed data.
-        //so we still have the whole address, including protocol (eg, websockets)
-        sbot.gossip.add(data, 'local')
+        // note: add the raw data, not the parsed data.
+        // so we still have the whole address, including protocol (eg, websockets)
+        ssbServer.gossip.add(data, 'local')
       }
     })
 
-    sbot.status.hook(function (fn) {
+    ssbServer.status.hook(function (fn) {
       var _status = fn()
-      if(!isEmpty(addrs)) {
+      if (!isEmpty(addrs)) {
         _status.local = {}
-        for(var k in addrs)
-          _status.local[k] = {address: addrs[k], seen: lastSeen[k]}
+        for (var k in addrs) {
+          _status.local[k] = { address: addrs[k], seen: lastSeen[k] }
+        }
       }
       return _status
     })
 
-    // broadcast self
-    setInterval(function () {
-      if(config.gossip && config.gossip.local === false)
-        return
-      // TODO: sign beacons, so that receipient can be confidant
-      // that is really your id.
-      // (which means they can update their peer table)
-      // Oh if this includes your local address,
-      // then it becomes unforgeable.
-      local.write(sbot.getAddress('private'))
-    }, 1000)
+    setImmediate(function () {
+      // broadcast self
+      var int = setInterval(function () {
+        if (config.gossip && config.gossip.local === false) return
+        // TODO: sign beacons, so that recipients can be confident
+        // that is really your id.
+        // (which means they can update their peer table)
+        // Oh if this includes your local address,
+        // then it becomes unforgeable.
+        var addr = ssbServer.getAddress('private') || ssbServer.getAddress('local')
+        if (addr) local.write(addr)
+      }, 1000)
+      if (int.unref) int.unref()
+    })
   }
 }
-
-
